cc6e6b0b61c621c7b90839d92b0b9cc18bd59dec
Oleg Dubrovsky <olegdubrovsky666@gmail.com>
2023-12-02 02:44:29 +0300

    Initial commit

---
+++ src/main/kotlin/01.kt
@@ -0,0 +1,69 @@
+import java.io.File
+
+
+val digits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    .withIndex()
+    .associateBy({ it.value }, { it.index + 1 })
+
+
+fun part1(input: List<String>): Int {
+    return input.fold(0) { acc: Int, line: String ->
+        var first = 0
+        while (!line[first].isDigit()) {
+            first++
+        }
+
+        var last = line.length - 1
+        while (!line[last].isDigit()) {
+            last--
+        }
+
+        acc + line[first].digitToInt() * 10 + line[last].digitToInt()
+    }
+}
+
+
+fun part2(input: List<String>): Int {
+    return input.fold(0) { acc: Int, line: String ->
+        var lineCalibrationValue = 0
+
+        var first = 0
+        while (first < line.length) {
+            if (line[first].isDigit()) {
+                lineCalibrationValue += line[first].digitToInt() * 10
+                break
+            } else {
+                val digit = digits.entries.firstOrNull { (s, _) -> line.substring(first).startsWith(s) }?.value
+                if (digit != null) {
+                    lineCalibrationValue += digit * 10
+                    break
+                }
+            }
+            first++
+        }
+
+        var last = line.length - 1
+        while (last >= 0) {
+            if (line[last].isDigit()) {
+                lineCalibrationValue += line[last].digitToInt()
+                break
+            } else {
+                val digit = digits.entries.firstOrNull { (s, _) -> line.substring(0, last + 1).endsWith(s) }?.value
+                if (digit != null) {
+                    lineCalibrationValue += digit
+                    break
+                }
+            }
+            last--
+        }
+
+        acc + lineCalibrationValue
+    }
+}
+
+
+fun main() {
+    val input = File("src/main/resources/input/01.txt").readLines()
+    println(part1(input))
+    println(part2(input))
+}
