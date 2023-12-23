e351414aa87a9b6174f78983bcadf80b5a0bc214
Oleg Dubrovsky <olegdubrovsky666@gmail.com>
2023-12-23 04:18:48 +0300

    add gradle

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
+}
